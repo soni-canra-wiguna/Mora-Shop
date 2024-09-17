056a97bc03dfde2d8dfd7ea65189b2575c7bcba5
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-17 07:36:41 +0700

    refactor: update instance prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import { PrismaLibSQL } from "@prisma/adapter-libsql"
+import { createClient } from "@libsql/client"
+import { env } from "@/env"
 
-import { env } from "@/env";
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+})
+
+const adapter = new PrismaLibSQL(libsql)
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+    adapter,
+  })
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+}
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? createPrismaClient()
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db
