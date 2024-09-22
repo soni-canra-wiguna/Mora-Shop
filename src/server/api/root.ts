814ace7f47ef151e312db3729dd34723b6684c61
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-22 08:39:56 +0700

    feat(api): add user API

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
-import { postRouter } from "@/server/api/routers/post"
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc"
-import { blogRouter } from "./routers/blog"
+import { userRouter } from "./routers/user"
 
 /**
  * This is the primary router for your server.
@@ -8,8 +7,7 @@ import { blogRouter } from "./routers/blog"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  blog: blogRouter,
+  user: userRouter,
 })
 
 // export type definition of API
