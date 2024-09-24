da3699e8de1b1471c01fff915a0b17d1c2681b8a
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-24 18:47:51 +0700

    feat(api): add product API

---
+++ src/app/api/product/[id]/route.ts
@@ -0,0 +1,79 @@
+import { db } from "@/lib/db"
+import { CreateProductRequest, productSchema } from "@/schema/product"
+import { Validation } from "@/schema/validation"
+import { NextRequest, NextResponse } from "next/server"
+import { z } from "zod"
+
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  try {
+    const { id } = params
+    const request: CreateProductRequest = await req.json()
+    const response = Validation.validate(productSchema.create, request)
+
+    await db.product.update({
+      where: {
+        id: id,
+      },
+      data: response,
+    })
+
+    return NextResponse.json(
+      {
+        message: "Successfully created Product",
+      },
+      { status: 201 },
+    )
+  } catch (error) {
+    console.log(error)
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation error",
+          errors: error.errors,
+        },
+        { status: 400 },
+      )
+    }
+    return NextResponse.json(
+      {
+        message: "internal server error",
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+}
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  try {
+    const { id } = params
+
+    await db.product.delete({
+      where: {
+        id,
+      },
+    })
+
+    return NextResponse.json(
+      { message: "product was deleted" },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+}
