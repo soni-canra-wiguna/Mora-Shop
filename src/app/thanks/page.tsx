08e417f4a30e4dcb0dc81fb3c4bc45e4f9b92bdc
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-24 23:38:06 +0700

    feat(ui): improve thanks page

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 
 export default function ThanksPage() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center gap-4">
+    <main className="flex min-h-screen w-full flex-col items-center gap-12">
       <img src="/thank-you.png" className="mt-10 size-28" />
-      <span className="whitespace-pre text-xl font-medium capitalize">
+      {/* <span className="whitespace-pre text-xl font-medium capitalize">
         thanks
-      </span>
-      <div className="flex items-center gap-2">
+      </span> */}
+      <div className="flex items-center gap-4">
         <Link href="/">
-          <Button variant="neutral">homepage</Button>
+          <Button variant="neutral">Homepage</Button>
         </Link>
         <Link href="/topup">
-          <Button>topup kembali</Button>
+          <Button className="capitalize">topup kembali</Button>
         </Link>
       </div>
     </main>
