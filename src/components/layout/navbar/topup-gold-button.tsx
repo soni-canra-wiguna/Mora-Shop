633658c21267c019124b8d4a1466daf808c8cd5a
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-24 11:20:51 +0700

    feat: fetch user data

--- src/components/layout/navbar/topup-gold-button.tsx
+++ src/components/layout/navbar/topup-gold-button.tsx
@@ -1,15 +1,29 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
+import { api } from "@/trpc/react"
+import { useAuth, useUser } from "@clerk/nextjs"
 import { Plus } from "lucide-react"
 import Link from "next/link"
 
 export const TopupGoldButton = () => {
+  const { userId } = useAuth()
+  const { isSignedIn } = useUser()
+  const { data, isPending } = api.user.getUser.useQuery(userId!, {
+    refetchInterval: 5000,
+  })
+
   return (
     <Link href="/topup">
       <Button className="h-fit p-2" variant="neutral">
         <div className="flex items-center gap-2">
           <img src="/coin.png" className="size-4" />
-          <span className="pr-4">100</span>
+          {!isSignedIn ? (
+            <span className="pr-4">0</span>
+          ) : (
+            <span className="pr-4">{isPending ? "..." : data?.gold}</span>
+          )}
         </div>
         <Separator orientation="vertical" />
         <Plus className="size-4" />
