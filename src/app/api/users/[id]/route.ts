53aa758aac35ff322a95858bfe9f94847d6d9a01
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-25 20:08:20 +0700

    feat(api): include purchase on user response API

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,6 +1,4 @@
 import { db } from "@/lib/db"
-import { UpdateUserRequest, userSchema } from "@/schema/user"
-import { Validation } from "@/schema/validation"
 import { NextRequest, NextResponse } from "next/server"
 
 export const GET = async (
@@ -14,46 +12,15 @@ export const GET = async (
       where: {
         userId: id,
       },
+      include: {
+        purchases: true,
+      },
     })
 
     return NextResponse.json(
       { message: "user was retrieved", data: user },
       { status: 200 },
     )
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json(
-      {
-        message: "Internal server error",
-      },
-      {
-        status: 500,
-      },
-    )
-  }
-}
-
-export const PATCH = async (
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) => {
-  try {
-    const { id } = params
-    const request: UpdateUserRequest = await req.json()
-    const { quantity } = Validation.validate(userSchema.update, request)
-
-    await db.user.update({
-      where: {
-        userId: id,
-      },
-      data: {
-        gold: {
-          decrement: quantity,
-        },
-      },
-    })
-
-    return NextResponse.json({ message: "gold was updated" }, { status: 200 })
   } catch (error) {
     console.log(error)
     return NextResponse.json(
