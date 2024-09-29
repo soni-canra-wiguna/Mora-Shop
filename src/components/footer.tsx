3e90405568b3617ddf21cffc02bcc6228db968bf
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-30 01:08:26 +0700

    feat(ui): improve footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,20 @@
+import { Github } from "lucide-react"
+import { Container } from "./layout/container"
+
 export const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <Container className="flex items-center justify-between py-8">
+        <span>
+          &copy; 2024 mora shop by{" "}
+          <span className="font-semibold capitalize text-main">
+            soni canra wiguna
+          </span>
+        </span>
+        <a href="https://github.com/soni-canra-wiguna" target="_blank">
+          <Github className="size-4 stroke-[1.5]" />
+        </a>
+      </Container>
+    </footer>
+  )
 }
